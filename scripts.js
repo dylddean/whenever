aff82510ec3e66a6c3ca53b7efd25ad4a91828bf
dylddean <103350950+dylddean@users.noreply.github.com>
2022-04-25 01:08:07 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function initAutocomplete() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.8688, lng: 151.2195 },
+    center: { lat: 38.680896, lng: -85.179398 },
     zoom: 13,
     mapTypeId: "roadmap",
   });
